c6932986d37dc2b2b846e6fc31ab4428becb15a8
Endriw Braga Lisboa <103139137+EndriwBraga@users.noreply.github.com>
2023-01-08 17:24:22 -0300

    animação da pagina ao usar o scroll

--- script.js
+++ script.js
@@ -52,3 +52,55 @@ initAccordion();
 
 /* scroll suave na navegação */
 
+function initScrollSuave(){
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+});
+}
+
+initScrollSuave();
+
+
+
+
+/* Animação scroll pag dela vir da esquerda p direita (mais ou menos isso) */
+
+function initAnimacaoScrollpag(){
+const sections = document.querySelectorAll('.js-scroll');
+
+if(sections.length){
+  const windowMetade = window.innerHeight * 0.5;
+
+  function animaScroll(){
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - windowMetade) < 0;
+
+      if(isSectionVisible) 
+          section.classList.add('ativo');
+      else 
+          section.classList.remove('ativo')
+    })
+  }
+
+animaScroll();
+
+window.addEventListener('scroll', animaScroll);
+  }
+}
+
+initAnimacaoScrollpag();
