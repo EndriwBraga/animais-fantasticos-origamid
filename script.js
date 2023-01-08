fe6940580017f81da2a1b541b48fecec3b7a4ac7
Endriw Braga Lisboa <103139137+EndriwBraga@users.noreply.github.com>
2023-01-08 15:58:31 -0300

    implementei o accordion list  no faq

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+function initTabNav() {
+
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
 /*verificação de se existe o conteudo na pagina */
 
-if(tabMenu.length){
+if(tabMenu.length && tabContent.length){
   tabContent[0].classList.add('ativo')
 }
 
@@ -21,4 +23,32 @@ tabMenu.forEach((itemMenu, index) => {
     activeTab(index);
   })
 })
+}
+
+initTabNav ();
+
+/* FAQ */
+
+function initAccordion(){
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
+  if(accordionList.length){
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion () {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    })
+  }
+}
+
+initAccordion();
+
+
+/* scroll suave na navegação */
 
