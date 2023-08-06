cf4804b0d4747a6fec3dbd6dccd02b46cd487e1f
Endriw Braga Lisboa <103139137+EndriwBraga@users.noreply.github.com>
2023-08-06 12:07:21 -0300

    <IMPLEMENTATION> Novo modal de login na pagina, optei por começar a usar modules no css>

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ import initScrollSuave from './js/modules/scroll-suave.js';
 import initAnimacaoScrollpag from './js/modules/scroll-animacao.js';
 import initAccordion from './js/modules/accordion.js';
 import initTabNav from './js/modules/tabnav.js';
+import initModal from './js/modules/modal.js';
 
 initScrollSuave();
 initAnimacaoScrollpag();
 initAccordion();
 initTabNav();
+initModal();
