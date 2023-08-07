33079f479c511a02f706c54da0c6e4c9b0e94b6c
Endriw Braga Lisboa <103139137+EndriwBraga@users.noreply.github.com>
2023-08-07 13:15:51 -0300

    <IMPLEMENTATION> Nova funcionalidade, pelo que eu entendi adicionando o evento 'addEventListener' e removendo quando clicar fora(aprendendo)

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -11,7 +11,33 @@ dropdownsMenus.forEach(menu =>{
 
 function handleClick(event){
     event.preventDefault();
-    this.classList.toggle('active');
+    this.classList.add('active');
+    outsideClick(this, ['touchstart','click'], () =>{
+        this.classList.remove('active');
+    });
+}
+
+function outsideClick(element, events, callback){
+    const html = document.documentElement;
+    const outside = 'data-outside';
+
+    if(!element.hasAttribute(outside)){
+        events.forEach(userEvent =>{
+            html.addEventListener(userEvent, handleOutsideClick);
+        })
+        
+        element.setAttribute(outside, '');
+    }
+    function handleOutsideClick(event){
+
+        if(!element.contains(event.target)){
+            element.removeAttribute(outside);
+            events.forEach(userEvent =>{
+                html.removeEventListener(userEvent, handleOutsideClick);
+            })
+            callback()
+    }
+    }
 }
 }
 
