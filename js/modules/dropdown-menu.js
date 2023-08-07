8272501251f71b7bb83996367c835ab42acdfa69
Endriw Braga Lisboa <103139137+EndriwBraga@users.noreply.github.com>
2023-08-07 16:50:52 -0300

    <REFACTOR> Adaptei o codigo do dropdown menu para o outsideclick por que dessa maneira posso utilizar no dropdown menu e no futuro no meu menu mobile

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,44 +1,25 @@
+import outsideClick from './outsideclick.js';
+
 export default function initDropDownMenu(){
-    
-const dropdownsMenus = document.querySelectorAll('[data-dropdown]');
+    const dropdownsMenus = document.querySelectorAll('[data-dropdown]');
 
 // menu.addEventListener(userEvent, handleClick, {passive:true}); //// Não posso usar por que to usando preventDefault e devia por causa da acessibilidade no touchstart.
-dropdownsMenus.forEach(menu =>{
-    ['touchstart','click'].forEach(userEvent =>{
-        menu.addEventListener(userEvent, handleClick);
-    })
-})
-
-function handleClick(event){
-    event.preventDefault();
-    this.classList.add('active');
-    outsideClick(this, ['touchstart','click'], () =>{
-        this.classList.remove('active');
-    });
-}
-
-function outsideClick(element, events, callback){
-    const html = document.documentElement;
-    const outside = 'data-outside';
-
-    if(!element.hasAttribute(outside)){
-        events.forEach(userEvent =>{
-            html.addEventListener(userEvent, handleOutsideClick);
+    dropdownsMenus.forEach(menu =>{
+        ['touchstart','click'].forEach(userEvent =>{
+            menu.addEventListener(userEvent, handleClick);
         })
-        
-        element.setAttribute(outside, '');
-    }
-    function handleOutsideClick(event){
+    })
 
-        if(!element.contains(event.target)){
-            element.removeAttribute(outside);
-            events.forEach(userEvent =>{
-                html.removeEventListener(userEvent, handleOutsideClick);
-            })
-            callback()
-    }
+    function handleClick(event){
+        event.preventDefault();
+        this.classList.add('active');
+        outsideClick(this, ['touchstart','click'], () =>{
+            this.classList.remove('active');
+        });
     }
 }
-}
 
 
+
+
+ 
